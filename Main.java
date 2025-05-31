0a908e895e2c26f767be78b69bbfa951bcab32ab
NatalyBobrova <bobruhha@gmail.com>
2025-05-31 19:26:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1: вывод имени
+        System.out.println("Nataly Bobrova");
+
+        // Задание 2: расчет баланса
+        int income = 44000;
+        int spending = 30000;
+        int balance = income - spending;
+
+        System.out.println("Итого (руб):");
+        System.out.println(balance);
+    }
+}
