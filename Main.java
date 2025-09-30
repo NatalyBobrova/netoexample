acaa1850f91d7dad1254c4e85e49a2edddd69a04
NatalyBobrova <bobruhha@gmail.com>
2025-10-01 00:44:55 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Задание 1: вывод имени
-        System.out.println("Nataly Bobrova");
+        Scanner scanner = new Scanner(System.in);
 
-        // Задание 2: расчет баланса
-        int income = 44000;
-        int spending = 30000;
-        int balance = income - spending;
+        System.out.print("Введите цену товара (в руб.): ");
+        int price = scanner.nextInt();
 
-        System.out.println("Итого (руб):");
-        System.out.println(balance);
+        System.out.print("Введите вес товара (в кг.): ");
+        int weight = scanner.nextInt();
+
+        int customsFee = calculateCustoms(price, weight);
+
+        System.out.println("Размер пошлины (в руб.) составит: " + customsFee);
+
+        scanner.close();
+    }
+
+    public static int calculateCustoms(int price, int weight) {
+        int fromPrice = price / 100;
+        int fromWeight = weight * 100;
+        return fromPrice + fromWeight;
     }
 }
