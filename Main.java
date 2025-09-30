c470c87f3f70d92bb4e938407c04c9e9952fa54b
NatalyBobrova <bobruhha@gmail.com>
2025-10-01 01:29:01 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,21 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Калькулятор");
-        Calculator calc = Calculator.instance.get();
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
 
-        int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
+        List<Integer> result = new ArrayList<>();
 
-        calc.println.accept(c);
+        for (Integer i : intList) {
+            if (i > 0 && i % 2 == 0) {
+                result.add(i);
+            }
+        }
 
-        int d = calc.multiply.apply(5, 6);
-        calc.println.accept(d);
+        Collections.sort(result);
 
-        int e = calc.pow.apply(4);
-        calc.println.accept(e);
-
-        boolean isPos = calc.isPositive.test(-5); // false
-        System.out.println("Число положительное? " + isPos);
-
-        System.out.println("\nРабочий");
-        OnTaskDoneListener listener = System.out::println;
-
-        Worker worker = new Worker(listener);
-        worker.start();
+        for (Integer i : result) {
+            System.out.println(i);
+        }
     }
 }
